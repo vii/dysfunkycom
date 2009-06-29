8cae0873c0452c08eea69002832f7cee99f8239d
John Fremlin <john@fremlin.org>
2009-06-29 08:56:14 +0000

    chaser-controller-touch

--- src/chaser.lisp
+++ src/chaser.lisp
@@ -16,13 +16,32 @@
 	       ;; (assert (not (minusp (sim-score new))))
 	       (values (sat-sx target) (sat-sy target)))))
     (loop do
-	 (when (funcall closing-condition)
+	 (when (funcall closing-condition sim)
 	   (return))
 	  (multiple-value-bind (sx sy)
 	      (pos-after-step)
 	    (cond ((> range (d sx sy))
 		   (loop repeat small-step do
 			 (sim-step sim)))
+		  (t
+		   (sim-step sim (/ sx step) (/ sy step))
+		   (loop repeat mini-step do (sim-step sim))))))))
+
+(defun chaser-controller-touch (sim 
+			  &key (target (sim-target sim)) (step 900) (range 800) 
+			  (mini-step 10))
+  (declare (optimize debug))
+  (labels ((pos-after-step ()
+	     (let* ((new (copy-sim sim)) 
+		    (target (sim-similar-sat new target)))
+	       (loop repeat step do (sim-step new))
+	       ;; (assert (not (minusp (sim-score new))))
+	       (values (sat-sx target) (sat-sy target)))))
+    (loop do
+	  (multiple-value-bind (sx sy)
+	      (pos-after-step)
+	    (cond ((> range (d sx sy))
+		   (return))
 		  (t
 		   (sim-step sim (/ sx step) (/ sy step))
 		   (loop repeat mini-step do (sim-step sim))))))))
