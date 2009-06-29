fd855bff674a84734d9bf2ae4c93fc40b572dae4
Peter Salvi <vukung@yahoo.com>
2009-06-29 14:59:37 +0900

    Some more problem 4 thingies...

--- src/control-problem-4.lisp
+++ src/control-problem-4.lisp
@@ -9,7 +9,36 @@
     (iter (for sat in sats)
 	  (finding sat minimizing (sat-distance sat us)))))
 
+(defun problem-4-jump (sim target &optional (steps 10))
+  (destructuring-bind (target-radius wait) 
+      (estimate-target-radius-iteratively sim target)
+    (push (list 0 0 target-radius) *show-orbits*)
+    (iter (repeat wait) (sim-step sim))
+
+    (problem-3-controller-jump sim target-radius)
+
+    (let ((us (sim-us sim)) (e (sim-target sim)))
+      (let ((vx (sat-vx us)) (vy (sat-vy us))
+	    (evx (sat-vx e)) (evy (sat-vy e)))
+	(sim-step sim (- vx evx) (- vy evy))))
+
+    (chaser-controller sim :target target :step steps)))
+
+(defun fuel-low-p (sim) ; TODO
+  (declare (ignore sim))
+  t)
+
+(defun refuel-controller (sim)
+  (let ((sim (copy-sim sim)))
+    (sim-step sim)
+    (format t "Fuel before meeting with station: ~a~%" (sim-fuel sim)))
+  (problem-1-controller sim (* 2 (sat-r (sim-fuelstation sim))))
+  (problem-4-jump sim (sim-fuelstation sim) 1)
+  (format t "Fuel after meeting with station: ~a~%" (sim-fuel sim)))
+
 (defun go-for-satellite (sat sim &optional (time 10) (max-thrust 1000))
+  (problem-4-jump sim sat)
+  #+nil
   (multiple-value-bind (x y)
       (sim-pos-at-time sim (sim-us sim) (+ (sim-time sim) time))
     (multiple-value-bind (ex ey)
@@ -25,25 +54,19 @@
 	      (sim-step sim (vx dir) (vy dir))
 	      (iter (repeat (1- time)) (sim-step sim))))))))
 
-(defun fuel-low-p (sim)
-  'TODO
-  nil)
-
-(defun refuel-controller (sim)
-  'TODO)
-
 (defun problem-4-controller (sim)
   (iter (with satellites = '(0 1 2 3 4 5 6 7 8 9 10))
 	(while satellites)
 	(for nearest = (nearest-satellite satellites sim))
+	(when (fuel-low-p sim)
+	  (refuel-controller sim))
 	(go-for-satellite nearest sim)
 	(for prev-score first 0 then score)
 	(for score = (sim-score sim))
-	(while (>= score 0))
-	(when (> score prev-score)
+	(when (/= score prev-score)
+	  (format t "New score: ~f~%" score)
 	  (setf satellites (remove nearest satellites)))
-	(when (fuel-low-p sim)
-	  (refuel-controller sim))
+	(while (>= score 0))
 	(finally (return (values (reverse (sim-thrusts sim)) (sim-time sim))))))
 
 
