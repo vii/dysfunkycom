a8901479fbbdc02fdaebe5a3bc813b6222958d5a
Peter Salvi <vukung@yahoo.com>
2009-06-29 16:25:11 +0900

    Some chaser updates.

--- src/chaser.lisp
+++ src/chaser.lisp
@@ -2,13 +2,14 @@
 
 (defun chaser-controller (sim 
 			  &key (target (sim-target sim)) (step 900) (range 500) (small-step 100)
-			  (closing-condition (lambda () (zerop (sim-score sim)))))
+			  (closing-condition (let ((original-score (sim-score sim)))
+					       (lambda () (= (sim-score sim) original-score)))))
   (declare (optimize debug))
   (labels ((pos-after-step ()
 	     (let* ((new (copy-sim sim)) 
 		    (target (sim-similar-sat new target)))
 	       (loop repeat step do (sim-step new))
-	       (assert (not (minusp (sim-score new))))
+	       ;; (assert (not (minusp (sim-score new))))
 	       (values (sat-sx target) (sat-sy target)))))
     (loop do
 	 (when (funcall closing-condition)
